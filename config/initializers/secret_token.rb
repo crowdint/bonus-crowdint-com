b4363fdd9f94b64dfbd2ce4692bd78f3508685ac
Mumo Carlos <mumo.crls@gmail.com>
2013-12-04 11:30:57 -0600

    [#61906994] Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BonusBatch::Application.config.secret_key_base = 'bfc2344b279a6fc9b026190f3cbb03a0200a66362c75976c21f95ba78fcb6e4fe7564abd9c95453612915da5b448b463724b0543b4a4a6e0ed54c1d661c45742'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BonusBatch::Application.config.session_store :cookie_store, key: '_bonus_batch_session'
