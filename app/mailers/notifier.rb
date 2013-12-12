1892d420907a189643e97d04947a1fec427b164e
Nora Alvarado <nora.alvarado@crowdint.com>
2013-12-12 10:02:37 -0600

    [#61904706] Add a promer default from on mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,4 +1,6 @@
 class Notifier < ActionMailer::Base
+  default from: 'bonus_batch_roboth@crowdint.com'
+
   def send_invite(to, organization)
     @organization = organization
     mail(to: to, subject: "You were invited to the #{organization.name}'s Bonus Batch")
