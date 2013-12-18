b4650a410f1b38554471fa94fb41820617d4a79e
Mumo Carlos <mumo.crls@gmail.com>
2013-12-18 10:19:47 -0600

    [#62682884] Validate positive bonus assigment

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -6,11 +6,15 @@ class Batch < ActiveRecord::Base
   has_many :bonuses
   belongs_to :organization
 
-  accepts_nested_attributes_for :user_batches, :bonuses
+  accepts_nested_attributes_for :user_batches
+  accepts_nested_attributes_for :bonuses, reject_if: :negative_amount
 
   private
   def default_allocation
     self.allocation ||= self.organization.total_allocation
   end
 
+  def negative_amount attributes
+    attributes['amount'] < 0
+  end
 end
