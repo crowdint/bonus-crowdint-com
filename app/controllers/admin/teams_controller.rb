203a2f5c88a43ec392aa76dbe66cd360b88d714b
David Padilla <david@crowdint.com>
2014-08-15 22:38:46 -0500

    Team CRUD

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -1,4 +1,6 @@
 class Admin::TeamsController < Admin::BaseController
+  before_filter :load_team, only: [:edit, :update, :destroy]
+
   def index
     @teams = Team.all
   end
@@ -16,8 +18,28 @@ class Admin::TeamsController < Admin::BaseController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @team.update_attributes(team)
+      redirect_to admin_teams_path, notice: 'The team was updated succesfully'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to admin_teams_path, notice: 'The team was deleted succesfully'
+  end
+
   private
   def team
     params.require(:team).permit(:name)
+  end
+
+  def load_team
+    @team = Team.find(params[:id])
   end
 end
--- features/admin_can_crud_teams.feature
+++ features/admin_can_crud_teams.feature
@@ -2,10 +2,17 @@ Feature: Admin can CRUD teams
   Background:
     Given I am logged in as an admin
 
-  Scenario: Create a team
+  Scenario: CRUD a team
     When I go to the "admin teams" page
     And I click on "Create Team"
     And I fill in "Name" with "Test Team"
     And I press "Create"
     Then I should be on the "admin teams" page
     And I should see "Test Team"
+    When I click on "Edit"
+    And I fill in "Name" with "Another Team Name"
+    And I press "Update"
+    Then I should see "Another Team Name"
+    When I click on "Delete"
+    Then I should not see "Another Team Name"
+
