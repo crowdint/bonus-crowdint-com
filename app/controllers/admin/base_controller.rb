c394d853c312bebd84769258c9faaeaffff6a3fe
David Padilla <david@crowdint.com>
2014-08-17 20:30:03 -0500

    Fix unauthorized redirection

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,6 @@ class Admin::BaseController < ApplicationController
   before_filter :authorize_admin
 
   def authorize_admin
-    redirect_to :root_path unless current_user.is_admin?
+    redirect_to :root, alert: "You are not authorized to access /admin" unless current_user.is_admin?
   end
 end
